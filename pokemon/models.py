e363f0d6bf5f4ed4c40d973254c2fb424116dd71
rfnkurle <45407665+rfnkurle@users.noreply.github.com>
2024-07-13 11:06:42 -0600

    migrations for model

--- pokemon/models.py
+++ pokemon/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Pokemon(models.Model):
     name = models.CharField(max_length=20),
-    power = models.CharField(max_lenght=20),
+    power = models.CharField(max_length=20),
     photo_url =  models.URLField()
 
     def __str__(self):
