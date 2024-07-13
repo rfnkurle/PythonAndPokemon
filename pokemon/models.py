7751d2d17da084a1c50885b03b5fae2ac95abd48
rfnkurle <45407665+rfnkurle@users.noreply.github.com>
2024-07-13 11:03:38 -0600

    added model

--- pokemon/models.py
+++ pokemon/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Pokemon(models.Model):
+    name = models.CharField(max_length=20),
+    power = models.CharField(max_lenght=20),
+    photo_url =  models.URLField()
+
+    def __str__(self):
+        return self.name
